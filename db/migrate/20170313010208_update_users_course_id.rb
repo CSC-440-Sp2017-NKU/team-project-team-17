d9a6aa3202cc4b5d4919f02e420e2f9eaafd7e2c
Ethan Perry <perrye1@nku.edu>
2017-03-13 05:47:10 +0000

    fixed a migration

--- db/migrate/20170313010208_update_users_course_id.rb
+++ db/migrate/20170313010208_update_users_course_id.rb
@@ -1,5 +1,5 @@
 class UpdateUsersCourseId < ActiveRecord::Migration[5.0]
   def change
-    change_column :users, :course_id, :integer
+    change_column :users, :course_id, 'integer USING CAST(course_id AS integer)'
   end
 end
