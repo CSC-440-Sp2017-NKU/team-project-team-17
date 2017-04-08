9e45cb731445467c5df99f26d158fd0ae2ab5545
Ethan Perry <perrye1@nku.edu>
2017-04-08 22:05:30 +0000

    working login form, checks existance of user in DB, and redirects to courses page. Also saves user id in session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
     def new
-  end
+    end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user
+      log_in user
+      redirect_to courses_path 
+    else
+      flash[:danger] = 'Invalid Email'
+      render 'new'
+    end
   end
 
   def destroy
