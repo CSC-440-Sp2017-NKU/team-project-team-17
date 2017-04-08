705d7ccf362b7f6f99cdfef954aadc74e50294eb
Ethan Perry <perrye1@nku.edu>
2017-04-08 23:54:08 +0000

    centralized navbar in the application layout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,21 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
+    end
+    
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
+    
+    def logged_in?
+        !current_user.nil?
+    end
+    
+    def user_is_admin?
+        #current_user.is_admin == true
+    end
+  
 end
