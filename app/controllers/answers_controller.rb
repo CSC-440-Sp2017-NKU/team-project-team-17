b7815a4bb9102de61da5cf443c4784124b1d2a8f
Ethan Perry <perrye1@nku.edu>
2017-04-30 03:31:13 +0000

    allow users to edit and delete their own questions and answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,5 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
-  before_filter :require_admin, only: [:edit]
 
   # GET /answers
   # GET /answers.json
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,5 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  before_filter :require_admin, only: [:edit]
 
   # GET /questions
   # GET /questions.json
