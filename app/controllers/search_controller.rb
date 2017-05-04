9ef635b5331cdbcadee0a787c9c1af4bdb833c2d
Ethan Perry <perrye1@nku.edu>
2017-05-04 06:48:33 +0000

    prevented blank search from being a wildcard

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,16 @@ class SearchController < ApplicationController
     def show
         
         @query = params[:query]
-        @questions = Question.select("questions.*, SUM(votes.score) score").joins("LEFT OUTER JOIN votes on votes.question_id = questions.id").group("questions.id")
-        @answers = Answer.all
-        @search_results = []
-        @questions.each do |q|
-            if (q.content.downcase.include? @query.downcase or q.title.downcase.include? @query.downcase)
-                @search_results << q
+        if @query.eql? ""
+            redirect_to :back
+        else
+            @questions = Question.select("questions.*, SUM(votes.score) score").joins("LEFT OUTER JOIN votes on votes.question_id = questions.id").group("questions.id")
+            @answers = Answer.all
+            @search_results = []
+            @questions.each do |q|
+                if (q.content.downcase.include? @query.downcase or q.title.downcase.include? @query.downcase)
+                    @search_results << q
+                end
             end
         end
     end
